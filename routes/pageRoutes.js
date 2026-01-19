4bc7f90b178e4a9ce915aaba5cb4800b2a7f76d1
checkybox <checkybox@proton.me>
2026-01-19 14:38:50 +0500

    sync dev branch to main

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -31,27 +31,6 @@ router.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/contact.html'));
 });
 
-// Search page with query parameter
-router.get('/search', (req, res) => {
-    const query = req.query.q;
-
-    if (!query) {
-        return res.status(400).send('<h2>Error: Search query parameter "q" is required.</h2><p>Example: /search?q=cpu</p>');
-    }
-
-    res.sendFile(path.join(__dirname, '../views/search.html'));
-});
-
-// Item detail page with route parameter
-router.get('/item/:id', (req, res) => {
-    const itemId = req.params.id;
-
-    if (!itemId) {
-        return res.status(400).send('<h2>Error: Item ID is required.</h2>');
-    }
-
-    res.sendFile(path.join(__dirname, '../views/item.html'));
-});
 
 export default router;
 
